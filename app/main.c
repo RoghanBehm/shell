d75bc95a66972bb5bf8de37fc1f2fa020c74b1d0
Roghan <roghanthomasbehm@gmail.com>
2024-12-17 05:07:28 +0000

    Return invalid command

--- app/main.c
+++ app/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   // Uncomment this block to pass the first stage
@@ -8,5 +9,11 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  char result[150];
+  input[strlen(input)-1]='\0';
+  snprintf(result, sizeof(result), "%s: command not found", input);
+  if (input) {
+    printf("%s\n", result); 
+  }
   return 0;
 }
